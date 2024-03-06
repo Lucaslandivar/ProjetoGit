70333615c38ee3378a86fd0046ebdd8a37e73725
Lucaslandivar <landivarlucas67@gmail.com>
2024-03-06 20:51:04 -0300

    Finish basic todo

--- JS/script.js
+++ JS/script.js
@@ -2,12 +2,25 @@
 
 const listInput = document.getElementById("listInput");
 const listBtn = document.getElementById("listBtn");
+const list = document.getElementById("lista");
 
 
 // !Funciones
+
+function addTask() {
+    if(listInput.value === ''){
+        alert("Você tem que adicionar uma tarefa!");
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = listInput.value;
+        list.appendChild(li);
+    }
+    listInput.value = "";
+}
 
 // !Eventos
 
 listBtn.addEventListener("click", (e) => {
     console.log("hello world!");
+    addTask();
 });

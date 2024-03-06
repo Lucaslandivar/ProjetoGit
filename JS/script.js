5d49201937b0bd31c5108bbb54fad923cbaa541a
Lucaslandivar <landivarlucas67@gmail.com>
2024-03-05 21:20:53 -0300

    Todo List teste

---
+++ JS/script.js
@@ -0,0 +1,13 @@
+// !Selección de los elementos
+
+const listInput = document.getElementById("listInput");
+const listBtn = document.getElementById("listBtn");
+
+
+// !Funciones
+
+// !Eventos
+
+listBtn.addEventListener("click", (e) => {
+    console.log("hello world!");
+});
